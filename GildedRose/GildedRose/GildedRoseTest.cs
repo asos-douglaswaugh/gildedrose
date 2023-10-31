6f4955d25b51ca61b91d54669f181c5c69d52675
Douglas Waugh <douglas.waugh@asos.com>
2023-10-31 16:15:48 +0000

    Verifies standard items degrade by one each day

--- GildedRose/GildedRose/GildedRoseTest.cs
+++ GildedRose/GildedRose/GildedRoseTest.cs
@@ -14,6 +14,16 @@ namespace GildedRose
         // Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but drops to 0 after the concert
         // Conjured items degrade in quality twice as fast as normal items
 
+        [TestCase("Standard item", 1, 1, 0)]
+        public void A_standard_item_should_degrade_by_one_each_day(string name, int startSellIn, int startQuality, int endQuality)
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = name, SellIn = startSellIn, Quality = startQuality } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.That(Items[0].Name, Is.EqualTo(name));
+            Assert.That(Items[0].Quality, Is.EqualTo(endQuality));
+        }
+
         [TestCase("Standard item", -1, 2, 0)]
         [TestCase("Standard item", 0, 2, 0)]
         public void Once_the_sell_by_date_has_passed_quality_degrades_twice_as_fast(string name, int startSellIn, int startQuality, int endQuality)
