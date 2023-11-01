88111e470793ccbcddb41cf2806986e9b1d80d8c
Douglas Waugh <douglas.waugh@asos.com>
2023-11-01 10:24:59 +0000

    Created an Aged Brie if statement, but broke the approval tests. Unit tests still passing

--- GildedRose/GildedRose/GildedRose.cs
+++ GildedRose/GildedRose/GildedRose.cs
@@ -15,6 +15,14 @@
                 if (Items[i].Name == "Sulfuras, Hand of Ragnaros")
                     continue;
 
+                if (Items[i].Name == "Aged Brie")
+                {
+                    if (Items[i].Quality < 50)
+                        Items[i].Quality = Items[i].Quality + 1;
+                    Items[i].SellIn = Items[i].SellIn - 1;
+                    continue;
+                }
+
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality > 0)
