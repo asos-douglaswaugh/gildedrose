ff0c68609852bd105f6c728b94fe4bf684a9aba0
Douglas Waugh <douglas.waugh@asos.com>
2023-11-01 10:16:53 +0000

    Move Sulfuras clause to the top the for loop in UpdateQuality

--- GildedRose/GildedRose/GildedRose.cs
+++ GildedRose/GildedRose/GildedRose.cs
@@ -12,6 +12,9 @@
         {
             for (var i = 0; i < Items.Count; i++)
             {
+                if (Items[i].Name == "Sulfuras, Hand of Ragnaros")
+                    continue;
+
                 if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality > 0)
