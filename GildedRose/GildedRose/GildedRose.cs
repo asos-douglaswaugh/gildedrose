39a9062606d1b7a5aaf270ad643174809e1caa2c
Douglas Waugh <douglas.waugh@asos.com>
2023-11-01 11:18:43 +0000

    Replace for loop with foreach

--- GildedRose/GildedRose/GildedRose.cs
+++ GildedRose/GildedRose/GildedRose.cs
@@ -10,69 +10,69 @@
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
+            foreach (var item in Items)
             {
-                if (Items[i].Name == "Sulfuras, Hand of Ragnaros")
+                if (item.Name == "Sulfuras, Hand of Ragnaros")
                     continue;
 
-                if (Items[i].Name == "Aged Brie")
+                if (item.Name == "Aged Brie")
                 {
-                    if (Items[i].Quality < 50)
-                        Items[i].Quality = Items[i].Quality + 1;
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                    if (Items[i].SellIn < 0 && Items[i].Quality < 50)
-                        Items[i].Quality = Items[i].Quality + 1;
+                    if (item.Quality < 50)
+                        item.Quality = item.Quality + 1;
+                    item.SellIn = item.SellIn - 1;
+                    if (item.SellIn < 0 && item.Quality < 50)
+                        item.Quality = item.Quality + 1;
                     continue;
                 }
 
-                if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    if (Items[i].Quality < 50)
+                    if (item.Quality < 50)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        item.Quality = item.Quality + 1;
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                         {
-                            if (Items[i].SellIn < 11)
+                            if (item.SellIn < 11)
                             {
-                                if (Items[i].Quality < 50)
+                                if (item.Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    item.Quality = item.Quality + 1;
                                 }
                             }
 
-                            if (Items[i].SellIn < 6)
+                            if (item.SellIn < 6)
                             {
-                                if (Items[i].Quality < 50)
+                                if (item.Quality < 50)
                                 {
-                                    Items[i].Quality = Items[i].Quality + 1;
+                                    item.Quality = item.Quality + 1;
                                 }
                             }
                         }
                     }
 
-                    Items[i].SellIn = Items[i].SellIn - 1;
+                    item.SellIn = item.SellIn - 1;
 
-                    if (Items[i].SellIn < 0)
+                    if (item.SellIn < 0)
                     {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        item.Quality = item.Quality - item.Quality;
                     }
 
                     continue;
                 }
 
-                if (Items[i].Quality > 0)
+                if (item.Quality > 0)
                 {
-                    Items[i].Quality = Items[i].Quality - 1;
+                    item.Quality = item.Quality - 1;
                 }
 
-                Items[i].SellIn = Items[i].SellIn - 1;
+                item.SellIn = item.SellIn - 1;
 
-                if (Items[i].SellIn < 0)
+                if (item.SellIn < 0)
                 {
-                    if (Items[i].Quality > 0)
+                    if (item.Quality > 0)
                     {
-                        Items[i].Quality = Items[i].Quality - 1;
+                        item.Quality = item.Quality - 1;
                     }
                 }
             }
