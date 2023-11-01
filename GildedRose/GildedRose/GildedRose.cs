dcfe21bac1edeb6f0ad83ce3d2a53b8effeca591
Douglas Waugh <douglas.waugh@asos.com>
2023-11-01 11:00:12 +0000

    Second attempt to put all the logic for backstage passes in an if statement and tests pass

--- GildedRose/GildedRose/GildedRose.cs
+++ GildedRose/GildedRose/GildedRose.cs
@@ -25,6 +25,42 @@
                     continue;
                 }
 
+                if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+
+                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (Items[i].SellIn < 11)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+
+                            if (Items[i].SellIn < 6)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+                        }
+                    }
+
+                    Items[i].SellIn = Items[i].SellIn - 1;
+
+                    if (Items[i].SellIn < 0)
+                    {
+                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                    }
+
+                    continue;
+                }
+
                 if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality > 0)
