f1143f54263f6d218d914413c3a65bcf7b603dc9
Douglas Waugh <douglas.waugh@asos.com>
2023-11-01 11:36:53 +0000

    Introduce SaleableItem object with strategy pattern for calculations

--- GildedRose/GildedRose/GildedRose.cs
+++ GildedRose/GildedRose/GildedRose.cs
@@ -3,60 +3,113 @@
     public class GildedRose
     {
         IList<Item> Items;
+        private List<SaleableItem> _saleableItems;
+
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
+
+            _saleableItems = new List<SaleableItem>();
+
+            foreach (var item in Items)
+            {
+                if (item.Name == "Sulfuras, Hand of Ragnaros")
+                    _saleableItems.Add(new SaleableItem(item, new Sulfuras()));
+                else if (item.Name == "Aged Brie")
+                    _saleableItems.Add(new SaleableItem(item, new AgedBrie()));
+                else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                    _saleableItems.Add(new SaleableItem(item, new BackstagePass()));
+                else
+                    _saleableItems.Add(new SaleableItem(item, new StandardItem()));
+            }
         }
 
         public void UpdateQuality()
         {
-            foreach (var item in Items)
+            foreach (var item in _saleableItems)
             {
-                if (item.Name == "Sulfuras, Hand of Ragnaros")
-                    continue;
-
-                if (item.Name == "Aged Brie")
-                {
-                    if (item.Quality < 50)
-                        item.Quality = item.Quality + 1;
-
-                    item.SellIn = item.SellIn - 1;
-
-                    if (item is { SellIn: < 0, Quality: < 50 })
-                        item.Quality = item.Quality + 1;
-
-                    continue;
-                }
-
-                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-
-                        if (item is { SellIn: < 11, Quality: < 50 })
-                            item.Quality = item.Quality + 1;
-
-                        if (item is { SellIn: < 6, Quality: < 50 })
-                            item.Quality = item.Quality + 1;
-                    }
-
-                    item.SellIn = item.SellIn - 1;
-
-                    if (item.SellIn < 0)
-                        item.Quality = item.Quality - item.Quality;
-
-                    continue;
-                }
-
-                if (item.Quality > 0)
-                    item.Quality = item.Quality - 1;
-
-                item.SellIn = item.SellIn - 1;
-
-                if (item is { SellIn: < 0, Quality: > 0 })
-                    item.Quality = item.Quality - 1;
+                item.UpdateQuality();
             }
+        }
+    }
+
+    public class StandardItem : IDegradable
+    {
+        public void UpdateQuality(Item item)
+        {
+            if (item.Quality > 0)
+                item.Quality = item.Quality - 1;
+
+            item.SellIn = item.SellIn - 1;
+
+            if (item is { SellIn: < 0, Quality: > 0 })
+                item.Quality = item.Quality - 1;
+        }
+    }
+
+    public class BackstagePass : IDegradable
+    {
+        public void UpdateQuality(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+
+                if (item is { SellIn: < 11, Quality: < 50 })
+                    item.Quality = item.Quality + 1;
+
+                if (item is { SellIn: < 6, Quality: < 50 })
+                    item.Quality = item.Quality + 1;
+            }
+
+            item.SellIn = item.SellIn - 1;
+
+            if (item.SellIn < 0)
+                item.Quality = item.Quality - item.Quality;
+        }
+    }
+
+    public class AgedBrie : IDegradable
+    {
+        public void UpdateQuality(Item item)
+        {
+            if (item.Quality < 50)
+                item.Quality = item.Quality + 1;
+
+            item.SellIn = item.SellIn - 1;
+
+            if (item is { SellIn: < 0, Quality: < 50 })
+                item.Quality = item.Quality + 1;
+        }
+    }
+
+    public class Sulfuras : IDegradable
+    {
+        public void UpdateQuality(Item item)
+        {
+            return;
+        }
+    }
+
+    public interface IDegradable
+    {
+        void UpdateQuality(Item item);
+    }
+
+    public class SaleableItem
+    {
+        private readonly Item _item;
+        private readonly IDegradable _sulfuras;
+
+        public SaleableItem(Item item, IDegradable sulfuras)
+        {
+            _item = item;
+            _sulfuras = sulfuras;
+        }
+
+        public void UpdateQuality()
+        {
+            _sulfuras.UpdateQuality(_item);
         }
     }
 }
