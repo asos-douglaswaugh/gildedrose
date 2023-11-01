9eb843512e31067b472cfe560681928756060687
Douglas Waugh <douglas.waugh@asos.com>
2023-11-01 10:46:38 +0000

    Remove all other references to aged brie

--- GildedRose/GildedRose/GildedRose.cs
+++ GildedRose/GildedRose/GildedRose.cs
@@ -25,7 +25,7 @@
                     continue;
                 }
 
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality > 0)
                     {
@@ -63,26 +63,16 @@
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Quality > 0)
                         {
-                            if (Items[i].Quality > 0)
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                            Items[i].Quality = Items[i].Quality - 1;
                         }
                     }
                     else
                     {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
+                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
                     }
                 }
             }
