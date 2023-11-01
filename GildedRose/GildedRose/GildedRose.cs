2668cc81e3083a1672704d9b1a5d555af9e45b05
Douglas Waugh <douglas.waugh@asos.com>
2023-11-01 11:02:28 +0000

    Tried to remove all other references of backstage passes but it broke a lot of tests

--- GildedRose/GildedRose/GildedRose.cs
+++ GildedRose/GildedRose/GildedRose.cs
@@ -61,54 +61,23 @@
                     continue;
                 }
 
-                if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (Items[i].Quality > 0)
                 {
-                    if (Items[i].Quality > 0)
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
+                    Items[i].Quality = Items[i].Quality - 1;
                 }
-                else
+
+                if (Items[i].Quality < 50)
                 {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
+                    Items[i].Quality = Items[i].Quality + 1;
                 }
 
                 Items[i].SellIn = Items[i].SellIn - 1;
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (Items[i].Quality > 0)
                     {
-                        if (Items[i].Quality > 0)
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        Items[i].Quality = Items[i].Quality - 1;
                     }
                 }
             }
