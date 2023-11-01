7de3224fe6d517d11ae0fbf1b40c98f6203f9805
Douglas Waugh <douglas.waugh@asos.com>
2023-11-01 11:02:35 +0000

    Revert previous changes to get back to green

--- GildedRose/GildedRose/GildedRose.cs
+++ GildedRose/GildedRose/GildedRose.cs
@@ -61,23 +61,54 @@
                     continue;
                 }
 
-                if (Items[i].Quality > 0)
+                if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
-                    Items[i].Quality = Items[i].Quality - 1;
+                    if (Items[i].Quality > 0)
+                    {
+                        Items[i].Quality = Items[i].Quality - 1;
+                    }
                 }
-
-                if (Items[i].Quality < 50)
+                else
                 {
-                    Items[i].Quality = Items[i].Quality + 1;
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+
+                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        {
+                            if (Items[i].SellIn < 11)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+
+                            if (Items[i].SellIn < 6)
+                            {
+                                if (Items[i].Quality < 50)
+                                {
+                                    Items[i].Quality = Items[i].Quality + 1;
+                                }
+                            }
+                        }
+                    }
                 }
 
                 Items[i].SellIn = Items[i].SellIn - 1;
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Quality > 0)
+                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        Items[i].Quality = Items[i].Quality - 1;
+                        if (Items[i].Quality > 0)
+                        {
+                            Items[i].Quality = Items[i].Quality - 1;
+                        }
+                    }
+                    else
+                    {
+                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
                     }
                 }
             }
