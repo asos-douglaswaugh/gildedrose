4d45ad804614e41b5a6fa9f66301e21c33d9af45
Douglas Waugh <douglas.waugh@asos.com>
2023-11-08 11:58:01 +0000

    Normalise formatting of new Items

--- GildedRose/GildedRose/Program.cs
+++ GildedRose/GildedRose/Program.cs
@@ -7,11 +7,36 @@
             Console.WriteLine("OMGHAI!");
 
             IList<Item> Items = new List<Item>{
-                new() {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new() {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new() {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new() {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new() {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new()
+                {
+                    Name = "+5 Dexterity Vest",
+                    SellIn = 10,
+                    Quality = 20
+                },
+                new()
+                {
+                    Name = "Aged Brie",
+                    SellIn = 2,
+                    Quality = 0
+                },
+                new()
+                {
+                    Name = "Elixir of the Mongoose",
+                    SellIn = 5,
+                    Quality = 7
+                },
+                new()
+                {
+                    Name = "Sulfuras, Hand of Ragnaros",
+                    SellIn = 0,
+                    Quality = 80
+                },
+                new()
+                {
+                    Name = "Sulfuras, Hand of Ragnaros",
+                    SellIn = -1,
+                    Quality = 80
+                },
                 new()
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
@@ -31,7 +56,12 @@
                     Quality = 49
                 },
                 // this conjured item does not work properly yet
-                new() {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                new()
+                {
+                    Name = "Conjured Mana Cake",
+                    SellIn = 3,
+                    Quality = 6
+                }
             };
 
             var app = new GildedRose(Items);
