5e88fbcf3d2d7a6a61c45475fcefca4e23255718
Douglas Waugh <douglas.waugh@asos.com>
2023-11-08 15:16:55 +0000

    Item implements interface IDegradable

--- GildedRose/GildedRose/Item.cs
+++ GildedRose/GildedRose/Item.cs
@@ -1,14 +1,20 @@
 ﻿namespace GildedRose
 {
-    public class Item
+    public class Item : IDegradable
     {
         public string Name { get; set; }
         public int SellIn { get; set; }
         public int Quality { get; set; }
+        public StandardItem Type { get; set; }
 
         public override string ToString()
         {
             return this.Name + ", " + this.SellIn + ", " + this.Quality;
+        }
+
+        public void UpdateQuality()
+        {
+            Type.UpdateQuality(this);
         }
     }
 }
